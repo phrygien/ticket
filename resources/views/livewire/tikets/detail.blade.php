6ee638a71bf465bbb8f40be0c10fef4d97f1953f
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-01-22 16:20:13 +0400

    modif detail ticket

--- resources/views/livewire/tikets/detail.blade.php
+++ resources/views/livewire/tikets/detail.blade.php
@@ -12,6 +12,8 @@ new class extends Component {
 
     use Toast;
 
+    public string $timezone;
+
     public int $ticketId;
     public array $ticketDetails = [];
     public bool $loading = true;
@@ -41,6 +43,7 @@ new class extends Component {
 
     public function mount($ticket)
     {
+        $this->timezone = $this->getTimezone(); // timezone navigateur
         $this->ticketId = $ticket;
         $this->fetchTicketDetails();
     }
@@ -804,7 +807,13 @@ new class extends Component {
                                                                                     </div>
                                                                                     <p class="text-xs text-gray-500">
                                                                                        <p class="text-xs text-gray-500">
-                                                                                            {{ isset($msg['date']) ? \Carbon\Carbon::parse(preg_replace('/\s*\([^)]+\)\s*$/', '', $msg['date']))->format('d/m/Y H:i') : '' }}
+
+                                                                                            <!-- {{ isset($msg['date']) ? \Carbon\Carbon::parse(preg_replace('/\s*\([^)]+\)\s*$/', '', $msg['date']))->format('d/m/Y H:i') : '' }} -->
+
+                                                                                            {{ 
+                                                                                                isset($msg['date']) ? \Carbon\Carbon::parse(preg_replace('/\s*\([^)]+\)\s*$/', '', $msg['date']))->setTimezone($timezone)->format('d/m/Y H:i') : ''
+                                                                                            }}
+
                                                                                             </p>
                                                                                     </p>
                                                                                 </div>
