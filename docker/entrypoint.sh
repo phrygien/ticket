abc6da23cf5510aaf90335079e03a958869e19ea
mecene phrygien <mecenephrygien@gmail.com>
2026-07-07 14:05:31 +0400

    update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
