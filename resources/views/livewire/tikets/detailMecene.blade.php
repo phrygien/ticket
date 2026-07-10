c9477ebaab8a571f5703184e8be8cb0e9581a36a
mecene phrygien <mecenephrygien@gmail.com>
2026-07-10 12:08:10 +0400

    test

--- resources/views/livewire/tikets/detailMecene.blade.php
+++ resources/views/livewire/tikets/detailMecene.blade.php
@@ -689,8 +689,8 @@ new class extends Component {
     <div class="mx-auto w-full mt-5">
         {{-- Tickets groupés (alignés avec les onglets) --}}
 
-        <livewire:tikets.group-ticket :ticket-id="$ticketId" :active-id="$ticketId" />
-
+            <livewire:tikets.group-ticket :ticket-id="$ticketId" :active-id="$ticketId" />
+        
         <!-- Navigation des onglets avec loading -->
         <div class="border-b border-gray-200 mb-5 mt-5">
             <nav class="-mb-px flex space-x-8">
