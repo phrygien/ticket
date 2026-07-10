aaeec7f267cfd82f6e4308969efab8e612af2d16
mecene phrygien <mecenephrygien@gmail.com>
2026-07-10 12:24:51 +0400

    Test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
             $middleware->alias(
             [
                 'jwt-session-auth' => EnsureTokenInSession::class
