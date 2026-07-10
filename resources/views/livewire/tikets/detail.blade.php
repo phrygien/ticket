c10757f25ed35ed4e4a3b4394a2a685b81ed5f5a
mecene phrygien <mecenephrygien@gmail.com>
2026-07-10 12:53:40 +0400

    Test

--- resources/views/livewire/tikets/detail.blade.php
+++ resources/views/livewire/tikets/detail.blade.php
@@ -23,9 +23,7 @@ new class extends Component {
 
     public ?array $selectedMessage = null;
 
-    //#[Validate(["photos.*" => "image|max:1024"])]
-    #[Validate(["photos.*" => "mimes:jpg,jpeg,png,gif,webp,heic,heif|max:1024"])]
-    public $photos = [];
+    #[Validate(["photos.*" => "image|max:5120"])] // 5 Mo max
     public $photos = [];
 
     public $newPhotos = [];
