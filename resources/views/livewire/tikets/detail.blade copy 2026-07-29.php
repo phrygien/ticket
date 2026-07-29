431c26134829f179b90756a7649d00cb647c5481
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-07-29 14:25:01 +0400

    ajout tab chat bot

--- resources/views/livewire/tikets/detail.blade copy 2026-07-29.php
+++ resources/views/livewire/tikets/detail.blade copy 2026-07-29.php
@@ -50,8 +50,6 @@ new class extends Component {
     public string $subject = '';
     public string $subjectOriginal = '';
 
-    public bool $chatbotMessageTab = true; // pour rendre visible le tab de chatbot
-
     public function mount($ticket)
     {
         $this->ticketId = $ticket;
@@ -710,23 +708,6 @@ new class extends Component {
                     Ticket - {{ $ticketDetails['details'][0]['num_ticket'] }}
                 </button>
 
-                @if($chatbotMessageTab)
-                    <button wire:click="setTab('chabotmessage')"
-                            class="px-3 py-2 text-sm font-medium whitespace-nowrap border-b-2 relative
-                {{ $activeTab === 'chabotmessage' ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200' }}">
-                        <svg wire:loading.remove wire:target="setTab('chabotmessage')" class="w-4 h-4 inline-block mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                        </svg>
-                        <svg wire:loading wire:target="setTab('chabotmessage')" class="animate-spin w-4 h-4 inline-block mr-1" fill="none" viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                        </svg>
-                        Conversation chatbot
-                    </button>
-                @endif
-
                 <button wire:click="setTab('conversation')"
                         class="px-3 py-2 text-sm font-medium whitespace-nowrap border-b-2 relative
             {{ $activeTab === 'conversation' ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200' }}">
@@ -785,8 +766,6 @@ new class extends Component {
                     <!-- Contenu description -->
                 @elseif($activeTab === 'conversation')
                     <!-- Contenu conversation -->
-                @elseif($activeTab === 'chabotmessage')
-                    <!-- Contenu chabotmessage -->
                 @elseif($activeTab === 'commentaire')
                     <!-- Contenu commentaire -->
                 @elseif($activeTab === 'sendmail')
@@ -1717,12 +1696,6 @@ new class extends Component {
                     </x-card>
                 @endif
             </div>
-
-        @if($activeTab === 'chabotmessage')
-            <div class="mx-auto max-w-5xl">
-                <p>Ici les message du chatbot</p>
-            </div>
-
         @endif
 
     </div>
