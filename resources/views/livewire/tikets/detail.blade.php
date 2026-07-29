effff82535810f41fd53d9c6f55b943c64c05bc6
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-07-29 17:08:02 +0400

    ajout tab chat bot

--- resources/views/livewire/tikets/detail.blade.php
+++ resources/views/livewire/tikets/detail.blade.php
@@ -1898,7 +1898,7 @@ new class extends Component {
                                         ? 'background-color: #3b82f6; color: white; border-bottom-left-radius: 0.25rem;'
                                         : 'background-color: white; color: #1f2937; border: 1px solid #e5e7eb; border-bottom-right-radius: 0.25rem;' }}
                                 ">
-                                    <p>{{ $this->formatMessage(trim($msg['message'])) }}</p>
+                                    <p>{{!! $this->formatMessage(trim($msg['message'])) !!}}</p>
                                 </div>
                             </div>
                         </div>
